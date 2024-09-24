1f397776be6ab805d7bf79a86010f1b5fab1e00c
Raza Saleemi <rks143@gmail.com>
2024-09-24 16:33:18 +0500

    feat(entity-framework): Created DBContext. Configure connection strings. Implement EFCore and use plugin based repository.

--- ServerManagement/Program.cs
+++ ServerManagement/Program.cs
@@ -1,15 +1,26 @@
+using Microsoft.EntityFrameworkCore;
 using ServerManagement.Components;
+using ServerManagement.Data;
+using ServerManagement.Models;
 using ServerManagement.StateStore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// AddDbContext is scoped. AddDbContextFactory is Singleton
+builder.Services.AddDbContextFactory<ServerManagementContext>( options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BlazorManagement"));
+});
 
 // Add services to the container.
 builder.Services.AddRazorComponents().
     AddInteractiveServerComponents();
 
 //builder.Services.AddTransient<SessionStorage>();
-//builder.Services.AddScoped<ContainerStorage>(); // use scoped for web server for privacy using signalR channel.
+builder.Services.AddScoped<ContainerStorage>(); // use scoped for web server for privacy using signalR channel.
 builder.Services.AddScoped<TorontoOnlineServersStore>();
+
+builder.Services.AddTransient<IServersEFCoreRepository, ServersEFCoreRepository>();
 
 var app = builder.Build();
 
