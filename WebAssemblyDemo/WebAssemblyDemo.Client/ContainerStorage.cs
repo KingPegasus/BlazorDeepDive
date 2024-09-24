dfa6c3a47ddb467fbd25164540fb58ab5fafb561
Raza Saleemi <rks143@gmail.com>
2024-09-24 12:17:23 +0500

    feat(web-assembly): Use DI container to maintain states for WASM components

---
+++ WebAssemblyDemo/WebAssemblyDemo.Client/ContainerStorage.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAssemblyDemo.Client
+{
+    public class ContainerStorage
+    {
+        private string _message = string.Empty;
+
+        public string GetMessage() { return _message; }
+
+        public void SetMessage(string Message) {  _message = Message; }
+    }
+}
--- WebAssemblyDemo/WebAssemblyDemo.Client/Program.cs
+++ WebAssemblyDemo/WebAssemblyDemo.Client/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using WebAssemblyDemo.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// DI ContainerStorage using Singleton so that it lives till client lifetime
+builder.Services.AddSingleton<ContainerStorage>();
 
 await builder.Build().RunAsync();
